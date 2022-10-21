6edfe25224b1d4f881afc73508a400f31e340e35
Galinka <galinka2002niko@gmail.com>
2022-10-21 08:49:23 +0300

    Изменение в коде

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
-public class Main {
-        public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Collections;
 
-            System.out.println("Welcome in my Java!");
+class MySort {
+    public static void main(String[] args) {
+        Integer[] arr = { 5, 2, 1, 8, 10 };
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int values : arr) {
+            System.out.print(values + ", ");
+            // 10, 8, 5, 2, 1,
         }
     }
+}
